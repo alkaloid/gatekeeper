09a70515d26a666267b7be7973a2a635bce1b449
Ben Langfeld <ben@langfeld.me>
2015-11-01 15:44:23 -0500

    Handle arbitrarily sized trailing packets

--- lib/gatekeeper/rfid_listener.ex
+++ lib/gatekeeper/rfid_listener.ex
@@ -23,16 +23,13 @@ defmodule Gatekeeper.RFIDListener do
 
   def handle_info({:elixir_serial, _serial, data}, {buffer, handler} = state) do
     Logger.debug "Received data from RFID reader: #{inspect data}"
+    leading = byte_size(data) - 1
     case data do
       <<2, rest :: binary>> ->
         Logger.info "Began receiving an RFID"
         IO.write(buffer, rest)
-      <<rest :: binary-size(1), 3>> ->
+      <<rest :: binary-size(leading), 3>> ->
         IO.write(buffer, rest)
-        id = buffer |> StringIO.flush |> String.rstrip
-        Logger.info "RFID card presented with ID #{id}"
-        send(handler, {:card_read, id})
-      <<3>> ->
         id = buffer |> StringIO.flush |> String.rstrip
         Logger.info "RFID card presented with ID #{id}"
         send(handler, {:card_read, id})
--- test/gatekeeper/rfid_listener_test.exs
+++ test/gatekeeper/rfid_listener_test.exs
@@ -39,6 +39,20 @@ defmodule Gatekeeper.RFIDListenerTest do
     send(listener, {:elixir_serial, self(), <<10, 3>>})
 
     assert_receive {:card_read, "7A0092D11F26"}
+
+    send(listener, {:elixir_serial, self(), <<2, 55>>})
+    send(listener, {:elixir_serial, self(), "A"})
+    send(listener, {:elixir_serial, self(), "00"})
+    send(listener, {:elixir_serial, self(), "9"})
+    send(listener, {:elixir_serial, self(), "2"})
+    send(listener, {:elixir_serial, self(), "D1"})
+    send(listener, {:elixir_serial, self(), "1"})
+    send(listener, {:elixir_serial, self(), "F"})
+    send(listener, {:elixir_serial, self(), "2"})
+    send(listener, {:elixir_serial, self(), "6"})
+    send(listener, {:elixir_serial, self(), <<13, 10, 3>>})
+
+    assert_receive {:card_read, "7A0092D11F26"}
   end
 
   test "reading multiple RFID cards sequentially", %{listener: listener} do
