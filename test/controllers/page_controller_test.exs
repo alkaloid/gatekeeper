a944444bd1951b6b2f810b5471bfb745af86a15a
Ben Klang <bklang@mojolingo.com>
2015-11-01 14:51:49 -0500

    Update test to match on this app

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Gatekeeper.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Alkaloid Gatekeeper"
   end
 end
