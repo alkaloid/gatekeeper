706981391667e0f5e7db5211f27b2ee91d5bdaf1
Ben Klang <bklang@mojolingo.com>
2015-11-01 10:39:46 -0500

    Add tests for inactive member

--- test/models/member_test.exs
+++ test/models/member_test.exs
@@ -2,6 +2,7 @@ defmodule Gatekeeper.MemberTest do
   use Gatekeeper.ModelCase
 
   alias Gatekeeper.Member
+  alias Gatekeeper.Company
 
   @valid_attrs %{active: true, email: "some content", name: "some content", phone: "some content"}
   @invalid_attrs %{}
@@ -14,5 +15,25 @@ defmodule Gatekeeper.MemberTest do
   test "changeset with invalid attributes" do
     changeset = Member.changeset(%Member{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "checking an active member with an active company" do
+    m = %Member{active: true, company: %Company{departure_date: nil}}
+    assert Member.active?(m)
+  end
+
+  test "checking an active member with an inactive company" do
+    m = %Member{active: true, company: %Company{departure_date: "2015-04-30 00:00:00"}}
+    refute Member.active?(m)
+  end
+
+  test "checking an inactive member with an active company" do
+    m = %Member{active: false, company: %Company{departure_date: nil}}
+    refute Member.active?(m)
+  end
+
+  test "checking an inactive member with an inactive company" do
+    m = %Member{active: false, company: %Company{departure_date: "2015-04-30 00:00:00"}}
+    refute Member.active?(m)
   end
 end
