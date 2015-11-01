e1f2b3dcc067c987692125703b1e8d5a696b0f5a
Ben Klang <bklang@mojolingo.com>
2015-11-01 11:45:42 -0500

    These tests pass now

--- test/models/rfid_token_test.exs
+++ test/models/rfid_token_test.exs
@@ -3,16 +3,14 @@ defmodule Gatekeeper.RfidTokenTest do
 
   alias Gatekeeper.RfidToken
 
-  @valid_attrs %{identifier: "some content"}
+  @valid_attrs %{identifier: "some content", active: false, member_id: 1}
   @invalid_attrs %{}
 
-  @tag :skip
   test "changeset with valid attributes" do
     changeset = RfidToken.changeset(%RfidToken{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  @tag :skip
   test "changeset with invalid attributes" do
     changeset = RfidToken.changeset(%RfidToken{}, @invalid_attrs)
     refute changeset.valid?
