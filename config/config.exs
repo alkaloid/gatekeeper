d7fd396e1d5667e373536930e451f81835b29091
Ben Langfeld <ben@langfeld.me>
2015-11-01 14:01:06 -0500

    Interface with a GPIO-controlled door lock

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,10 @@ config :logger, :console,
 config :gatekeeper, :rfidreader,
   device: "/dev/ttyUSB0"
 
+config :gatekeeper, :doorlock,
+  gpio_port: '2',
+  type: Gatekeeper.DoorLock.Dummy
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
