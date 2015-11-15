a46e4669c14285de26cbd5062217d8cc4fd8158f
Ben Klang <bklang@mojolingo.com>
2015-11-15 14:24:41 -0500

    Fix missing aliases

--- test/models/rfid_token_test.exs
+++ test/models/rfid_token_test.exs
@@ -2,6 +2,8 @@ defmodule Gatekeeper.RfidTokenTest do
   use Gatekeeper.ModelCase
   import Gatekeeper.Factory
 
+  alias Gatekeeper.Company
+  alias Gatekeeper.Member
   alias Gatekeeper.RfidToken
 
   @valid_attrs %{identifier: "some content", active: false, member_id: 1}
