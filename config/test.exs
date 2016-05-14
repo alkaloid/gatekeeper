f696ecc87317daa5a84dd4971c96e9a8fadfd3dc
Ben Klang <bklang@mojolingo.com>
2016-05-13 22:38:32 -0400

    Provide a secret_key_base to help tests pass

--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,7 @@ use Mix.Config
 # you can enable the server option below.
 config :gatekeeper, Gatekeeper.Endpoint,
   http: [port: 4001],
+  secret_key_base: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
   server: false
 
 # Print only warnings and errors during test
