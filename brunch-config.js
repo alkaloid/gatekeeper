f68240b18fe2060edcbd02e88fefa1a7c78b54af
Ben Klang <ben@alkaloid.net>
2016-07-24 23:17:33 -0400

    Upgrade to Bootstrap from NPM
    Following instructions from https://gist.github.com/scrogson/e72f40c3c8e83c16c4b8

--- brunch-config.js
+++ brunch-config.js
@@ -52,17 +52,31 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    copycat: {
+      "fonts": ["node_modules/bootstrap-sass/assets/fonts/bootstrap"] // copy node_modules/bootstrap-sass/assets/fonts/bootstrap/* to priv/static/fonts/
+    },
+    sass: {
+      includePaths: ["node_modules/bootstrap-sass/assets/stylesheets"], // tell sass-brunch where to look for files to @import
+      precision: 8 // minimum precision required by bootstrap-sass
     }
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
+      "js/app.js": [
+        "bootstrap-sass", // require bootstrap-sass' JavaScript globally
+        "web/static/js/app"
+      ]
     }
   },
 
   npm: {
     enabled: true,
-    whitelist: ["phoenix", "phoenix_html"]
+    whitelist: ["phoenix", "phoenix_html", "jquery", "bootstrap-sass"],  // pull jquery and bootstrap-sass in as front-end assets
+    globals: { // bootstrap-sass' JavaScript requires both '$' and 'jQuery' in global scope
+      $: 'jquery',
+      jQuery: 'jquery'
+    }
   }
 };
