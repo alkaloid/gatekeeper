59ed2e4124f83c59ac4f89840eaa1e3e0ee88b6e
Ben Klang <ben@alkaloid.net>
2017-09-03 14:34:21 -0400

    Socket updates for newer Phoenix version

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,11 +12,10 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-import {Socket} from "phoenix"
 
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+import socket from "./socket"
